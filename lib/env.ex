e114b3139563c84ea073338b884f85ef045cefa7
cavis <ryancavis@gmail.com>
2017-04-12 16:27:21 -0600

    More fixes for compiled ENVs

--- lib/env.ex
+++ lib/env.ex
@@ -1,7 +1,7 @@
 defmodule Env do
 
   def get(key) do
-    get_dot_env(key) || get_env_config(key)
+    get_env_config(key)
   end
 
   defp get_dot_env(key) do
@@ -9,19 +9,29 @@ defmodule Env do
     |> Atom.to_string
     |> String.upcase
     |> System.get_env
-    |> fix_dotenv_newlines
+    |> fix_newlines
+    |> fix_quotes
+    |> defined_value
+  end
+
+  defp get_env_config(key) do
+    Application.get_env(:castle, :env_config)[key]
+    |> fix_newlines
+    |> fix_quotes
+    |> defined_value
   end
 
   # multiline .env values will have double encoded newlines
-  defp fix_dotenv_newlines("" <> val), do: String.replace(val, "\\n", "\n")
-  defp fix_dotenv_newlines(val), do: val
+  defp fix_newlines("" <> val), do: String.replace(val, "\\n", "\n")
+  defp fix_newlines(val), do: val
 
-  defp get_env_config(key) do
-    Application.get_env(:castle, :env_config)[key] |> defined_value
-  end
+  # depending on environment (AWS ECS vs docker-compose) some things are quoted
+  defp fix_quotes("\"" <> rest), do: String.replace(rest, ~r/"$/, "")
+  defp fix_quotes(val), do: val
 
   # don't allow uncompiled "${KEY}" values
-  defp defined_value("${" <> _rest), do: nil
-  defp defined_value(val), do: val
+  def defined_value("${" <> _rest), do: nil
+  def defined_value(""), do: nil
+  def defined_value(val), do: val
 
 end
