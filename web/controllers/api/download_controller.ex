9e1f036d2fb0b3b47905a5e86416c0605f073276
cavis <ryancavis@gmail.com>
2017-04-12 11:32:26 -0600

    Short downloads cache

--- web/controllers/api/download_controller.ex
+++ web/controllers/api/download_controller.ex
@@ -1,11 +1,20 @@
 defmodule Porter.API.DownloadController do
   use Porter.Web, :controller
 
+  alias Porter.Redis.CachedResponse, as: Redis
+
+  plug Porter.Plugs.ParseInt, "podcast_id"
+
+  @downloads_ttl 15
+
   def index(conn, %{"podcast_id" => podcast_id}) do
     %{assigns: %{time_from: from, time_to: to, interval: interval}} = conn
-    {data, meta} = BigQuery.podcast_downloads(podcast_id, from, to, interval)
+
+    fetch = fn() -> BigQuery.podcast_downloads(podcast_id, from, to, interval) end
+    {data, meta} = Redis.cached key("podcast", podcast_id, from, to, interval), @downloads_ttl, fetch
+
     render conn, "podcast.json",
-      id: String.to_integer(podcast_id),
+      id: podcast_id,
       interval: interval,
       downloads: data,
       meta: meta
@@ -14,11 +23,21 @@ defmodule Porter.API.DownloadController do
   def index(conn, %{"episode_guid" => episode_guid}) do
     %{assigns: %{time_from: from, time_to: to, interval: interval}} = conn
 
-    {data, meta} = BigQuery.episode_downloads(episode_guid, from, to, interval)
+    fetch = fn() -> BigQuery.episode_downloads(episode_guid, from, to, interval) end
+    {data, meta} = Redis.cached key("episode", episode_guid, from, to, interval), @downloads_ttl, fetch
+
     render conn, "episode.json",
       guid: episode_guid,
       interval: interval,
       downloads: data,
       meta: meta
+  end
+
+  defp key(type, id, from, to, interval) do
+    if Timex.compare(to, Timex.now) > 0 do
+      "#{type}.downloads.#{id}.#{from}.now.#{interval}"
+    else
+      "#{type}.downloads.#{id}.#{from}.#{to}.#{interval}"
+    end
   end
 end
