3b69f79f2b89b8bc61946217536a04a710106f44
cavis <ryancavis@gmail.com>
2017-04-12 11:34:27 -0600

    Short impressions cache

--- web/controllers/api/impression_controller.ex
+++ web/controllers/api/impression_controller.ex
@@ -1,11 +1,20 @@
 defmodule Porter.API.ImpressionController do
   use Porter.Web, :controller
 
+  alias Porter.Redis.CachedResponse, as: Redis
+
+  plug Porter.Plugs.ParseInt, "podcast_id"
+
+  @impressions_ttl 15
+
   def index(conn, %{"podcast_id" => podcast_id}) do
     %{assigns: %{time_from: from, time_to: to, interval: interval}} = conn
-    {data, meta} = BigQuery.podcast_impressions(podcast_id, from, to, interval)
+
+    fetch = fn() -> BigQuery.podcast_impressions(podcast_id, from, to, interval) end
+    {data, meta} = Redis.cached key("podcast", podcast_id, from, to, interval), @impressions_ttl, fetch
+
     render conn, "podcast.json",
-      id: String.to_integer(podcast_id),
+      id: podcast_id,
       interval: interval,
       impressions: data,
       meta: meta
@@ -14,11 +23,21 @@ defmodule Porter.API.ImpressionController do
   def index(conn, %{"episode_guid" => episode_guid}) do
     %{assigns: %{time_from: from, time_to: to, interval: interval}} = conn
 
-    {data, meta} = BigQuery.episode_impressions(episode_guid, from, to, interval)
+    fetch = fn() -> BigQuery.episode_impressions(episode_guid, from, to, interval) end
+    {data, meta} = Redis.cached key("episode", episode_guid, from, to, interval), @impressions_ttl, fetch
+
     render conn, "episode.json",
       guid: episode_guid,
       interval: interval,
       impressions: data,
       meta: meta
+  end
+
+  defp key(type, id, from, to, interval) do
+    if Timex.compare(to, Timex.now) > 0 do
+      "#{type}.impressions.#{id}.#{from}.now.#{interval}"
+    else
+      "#{type}.impressions.#{id}.#{from}.#{to}.#{interval}"
+    end
   end
 end
