9a14dfc8a97c7f14214e051c416963c05033004a
cavis <ryancavis@gmail.com>
2017-04-13 09:00:07 -0600

    Remove debug

--- lib/castle.ex
+++ lib/castle.ex
@@ -18,7 +18,6 @@ defmodule Castle do
     redix_config =
       [host: Env.get(:redis_host), port: Env.get(:redis_port)]
       |> Enum.filter(fn({_key, val}) -> val end)
-    IO.inspect Env.get(:redis_pool_size)
     redix_size = 5
     redix_workers = for i <- 0..(redix_size - 1) do
       worker(Redix, [redix_config, [name: :"redix_#{i}"]], id: {Redix, i})
