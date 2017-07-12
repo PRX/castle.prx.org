0128b306e7df5479bb4b638c2fc844ff8751dde7
cavis <ryancavis@gmail.com>
2017-07-12 16:00:34 -0600

    Fix external tests for staging tables

--- test/big_query/episodes_test.exs
+++ test/big_query/episodes_test.exs
@@ -5,36 +5,36 @@ defmodule Castle.BigQueryEpisodesTest do
 
   @tag :external
   test "lists episodes" do
-    {result, _meta} = list(Timex.to_datetime(~D[2017-04-09]))
+    {result, _meta} = list(Timex.to_datetime(~D[2017-06-28]))
     assert is_list result
     assert length(result) > 10
     assert hd(result).feeder_episode
 
     assert hd(result).downloads_past1 > 1
-    assert hd(result).downloads_past12 > hd(result).downloads_past1
-    assert hd(result).downloads_past24 > hd(result).downloads_past12
-    assert hd(result).downloads_past48 > hd(result).downloads_past24
+    assert hd(result).downloads_past12 >= hd(result).downloads_past1
+    assert hd(result).downloads_past24 >= hd(result).downloads_past12
+    assert hd(result).downloads_past48 >= hd(result).downloads_past24
 
     assert hd(result).impressions_past1 > 1
-    assert hd(result).impressions_past12 > hd(result).impressions_past1
-    assert hd(result).impressions_past24 > hd(result).impressions_past12
-    assert hd(result).impressions_past48 > hd(result).impressions_past24
+    assert hd(result).impressions_past12 >= hd(result).impressions_past1
+    assert hd(result).impressions_past24 >= hd(result).impressions_past12
+    assert hd(result).impressions_past48 >= hd(result).impressions_past24
   end
 
   @tag :external
   test "shows an episode" do
-    {result, _meta} = show("66e048bf-5bb5-4818-be56-729a91e8f777", Timex.to_datetime(~D[2017-04-09]))
+    {result, _meta} = show("7acf74b8-7b0a-4e9e-90be-f69052064b77", Timex.to_datetime(~D[2017-06-28]))
     assert is_map result
-    assert result.feeder_episode == "66e048bf-5bb5-4818-be56-729a91e8f777"
+    assert result.feeder_episode == "7acf74b8-7b0a-4e9e-90be-f69052064b77"
 
     assert result.downloads_past1 > 1
-    assert result.downloads_past12 > result.downloads_past1
-    assert result.downloads_past24 > result.downloads_past12
-    assert result.downloads_past48 > result.downloads_past24
+    assert result.downloads_past12 >= result.downloads_past1
+    assert result.downloads_past24 >= result.downloads_past12
+    assert result.downloads_past48 >= result.downloads_past24
 
     assert result.impressions_past1 > 1
-    assert result.impressions_past12 > result.impressions_past1
-    assert result.impressions_past24 > result.impressions_past12
-    assert result.impressions_past48 > result.impressions_past24
+    assert result.impressions_past12 >= result.impressions_past1
+    assert result.impressions_past24 >= result.impressions_past12
+    assert result.impressions_past48 >= result.impressions_past24
   end
 end
--- test/big_query/impressions_test.exs
+++ test/big_query/impressions_test.exs
@@ -5,27 +5,27 @@ defmodule Castle.BigQueryImpressionsTest do
 
   @tag :external
   test "lists impressions for a podcast" do
-    {:ok, start, _} = DateTime.from_iso8601("2017-04-09T21:45:00Z")
-    {:ok, finish, _} = DateTime.from_iso8601("2017-04-10T04:15:00Z")
-    {result, _meta} = for_podcast(45, start, finish, 900)
+    {:ok, start, _} = DateTime.from_iso8601("2017-06-27T21:45:00Z")
+    {:ok, finish, _} = DateTime.from_iso8601("2017-06-28T04:15:00Z")
+    {result, _meta} = for_podcast(57, start, finish, 900)
 
     assert is_list result
     assert length(result) == 26
     assert hd(result).time
-    assert Timex.to_unix(hd(result).time) == 1491774300
+    assert Timex.to_unix(hd(result).time) == 1498599900
     assert hd(result).count > 0
   end
 
   @tag :external
   test "lists impressions for an episode" do
-    {:ok, start, _} = DateTime.from_iso8601("2017-04-09T21:45:00Z")
-    {:ok, finish, _} = DateTime.from_iso8601("2017-04-10T04:15:00Z")
-    {result, _meta} = for_episode("66e048bf-5bb5-4818-be56-729a91e8f777", start, finish, 3600)
+    {:ok, start, _} = DateTime.from_iso8601("2017-06-27T21:45:00Z")
+    {:ok, finish, _} = DateTime.from_iso8601("2017-06-28T04:15:00Z")
+    {result, _meta} = for_episode("7acf74b8-7b0a-4e9e-90be-f69052064b77", start, finish, 3600)
 
     assert is_list result
     assert length(result) == 8
     assert hd(result).time
-    assert Timex.to_unix(hd(result).time) == 1491771600
+    assert Timex.to_unix(hd(result).time) == 1498597200
     assert hd(result).count > 0
   end
 end
