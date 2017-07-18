2954d5088da02a40e4a7938f96a53d897286c327
cavis <ryancavis@gmail.com>
2017-07-18 16:31:56 -0600

    Fix typos

--- web/views/api/impression_view.ex
+++ web/views/api/impression_view.ex
@@ -15,7 +15,7 @@ defmodule Castle.API.ImpressionView do
     %{guid: guid} |> meta_json(data) |> counts_json(data)
   end
 
-  def render("podcast-episode.json", %{guid: guid} = data) do
+  def render("episode-group.json", %{guid: guid} = data) do
     %{guid: guid} |> meta_json(data) |> groups_json(data)
   end
 end
