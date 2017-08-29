6cf1db466bc7bf0b7db592c57b4d8fdfcec1dd2b
cavis <ryancavis@gmail.com>
2017-08-29 11:10:22 -0600

    Stub partition functions in fake redis

--- test/support/fake_redis.ex
+++ test/support/fake_redis.ex
@@ -4,4 +4,10 @@ defmodule Castle.FakeRedis do
   def cached(_key, _ttl, work_fn), do: work_fn.()
   def interval(_key_prefix, from, _to, _interval, work_fn), do: work_fn.(from)
   def interval(_key_prefix, intv, work_fn), do: work_fn.(intv)
+
+  # TODO: these aren't as easy
+  def partition(_key_prefix, _worker_fns), do: {[], %{fake: true}}
+  def partition(_key_prefix, _combiner_fn, _worker_fns), do: {[], %{fake: true}}
+  def partition_get(_key_prefix, _num_parts), do: {[], %{fake: true}}
+  def partition_get(_key_prefix, _num_parts, _combiner_fn), do: {[], %{fake: true}}
 end
