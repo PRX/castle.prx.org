e31e1ad1e3c012c3a4328b457f60147f41067b6c
cavis <ryancavis@gmail.com>
2017-11-06 12:55:38 -0700

    Modify ranges for new inclusivityness

--- test/big_query/downloads_test.exs
+++ test/big_query/downloads_test.exs
@@ -7,7 +7,7 @@ defmodule Castle.BigQueryDownloadsTest do
   import BigQuery.Downloads
 
   test "lists downloads for all podcasts" do
-    intv = interval("2017-06-27T21:45:00Z", "2017-06-28T04:15:00Z", BigQuery.TimestampRollups.QuarterHourly)
+    intv = interval("2017-06-27T21:45:00Z", "2017-06-28T04:00:00Z", BigQuery.TimestampRollups.QuarterHourly)
     {result, _meta} = for_podcasts(intv)
 
     assert is_list result
@@ -21,7 +21,7 @@ defmodule Castle.BigQueryDownloadsTest do
   end
 
   test "groups downloads for a podcast" do
-    intv = interval("2017-07-10T21:45:00Z", "2017-07-11T04:15:00Z", BigQuery.TimestampRollups.QuarterHourly)
+    intv = interval("2017-07-10T21:45:00Z", "2017-07-11T04:00:00Z", BigQuery.TimestampRollups.QuarterHourly)
     group = Castle.Plugs.Group.get("city", 3)
     {result, _meta} = group_podcast(57, intv, group)
 
@@ -33,6 +33,7 @@ defmodule Castle.BigQueryDownloadsTest do
     assert_time result, 2, "2017-07-10T21:45:00Z"
     assert_time result, 3, "2017-07-10T21:45:00Z"
     assert_time result, 4, "2017-07-10T22:00:00Z"
+    assert_time result, 26 * 4 - 1, "2017-07-11T04:00:00Z"
 
     assert Enum.at(result, 0).display == nil
     assert Enum.at(result, 1).display != nil
@@ -45,7 +46,7 @@ defmodule Castle.BigQueryDownloadsTest do
   end
 
   test "lists downloads for all episodes" do
-    intv = interval("2017-06-27T21:45:00Z", "2017-06-28T04:15:00Z", BigQuery.TimestampRollups.QuarterHourly)
+    intv = interval("2017-06-27T21:45:00Z", "2017-06-28T04:00:00Z", BigQuery.TimestampRollups.QuarterHourly)
     {result, _meta} = for_episodes(intv)
 
     assert is_list result
@@ -61,7 +62,7 @@ defmodule Castle.BigQueryDownloadsTest do
   end
 
   test "groups downloads for an episode" do
-    intv = interval("2017-07-10T21:45:00Z", "2017-07-11T04:15:00Z", BigQuery.TimestampRollups.QuarterHourly)
+    intv = interval("2017-07-10T21:45:00Z", "2017-07-11T04:00:00Z", BigQuery.TimestampRollups.QuarterHourly)
     group = Castle.Plugs.Group.get("country", 2)
     {result, _meta} = group_episode("7acf74b8-7b0a-4e9e-90be-f69052064b77", intv, group)
 
