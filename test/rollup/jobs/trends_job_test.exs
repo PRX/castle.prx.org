a69494d1019a2db941743204d2d7dfbd588f45b0
cavis <ryancavis@gmail.com>
2018-02-06 15:59:13 -0700

    Test for ttl

--- test/rollup/jobs/trends_job_test.exs
+++ test/rollup/jobs/trends_job_test.exs
@@ -1,5 +1,6 @@
 defmodule Castle.Rollup.Jobs.TrendsTest do
   use Castle.BigQueryCase, async: true
+  use Castle.TimeHelpers
 
   import Castle.Rollup.Jobs.Trends
 
@@ -13,5 +14,10 @@ defmodule Castle.Rollup.Jobs.TrendsTest do
     ]
     results = combine(datas)
     assert results == datas
+  end
+
+  test "uses a TTL at the end of today" do
+    assert ttl_eod(get_dtim("2017-03-22T00:00:00Z")) == 86400
+    assert ttl_eod(get_dtim("2017-03-22T23:59:59Z")) == 1
   end
 end
