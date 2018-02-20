3ee119ff6da583d94af7ebb9dadfb9a558bf20d4
cavis <ryancavis@gmail.com>
2018-02-20 12:47:50 -0700

    Refactor trends/totals to use the interval cache keys

--- lib/rollup/jobs/totals_job.ex
+++ lib/rollup/jobs/totals_job.ex
@@ -1,67 +1,44 @@
 defmodule Castle.Rollup.Jobs.Totals do
-  # TODO: import "public" redis api from the application
-  import Castle.Redis.PartitionCache
+  import Castle.Redis.HashCache
 
-  # old - all non-today partitions from the beginning of time - 15 day cache
-  # mid - intermediate query to pick up the slack - 1 day cache
-  # new - always the current-date-partition - 5 minute cache
+  def podcasts_key(), do: "rollups.totals.podcasts"
+  def episodes_key(), do: "rollups.totals.episodes"
+
   def run() do
-    partition "rollup.totals", &Castle.Rollup.Jobs.Totals.combine/1, [
-      {1296000, &Castle.Rollup.Jobs.Totals.old_part/0},
-      {86400, &Castle.Rollup.Jobs.Totals.mid_part/1},
-      {300, &Castle.Rollup.Jobs.Totals.new_part/1}]
+    t1 = Task.async(fn ->
+      hash_cache podcasts_key(), &Castle.Rollup.Jobs.Totals.query_podcasts/2
+    end)
+    t2 = Task.async(fn ->
+      hash_cache episodes_key(), &Castle.Rollup.Jobs.Totals.query_episodes/2
+    end)
+    {Task.await(t1), Task.await(t2)}
   end
 
-  def get() do
-    partition_get "rollup.totals", 3, &Castle.Rollup.Jobs.Totals.combine/1
+  def query_podcasts(from, to), do: query("feeder_podcast", from, to)
+  def query_episodes(from, to), do: query("feeder_episode", from, to)
+
+  defp query(_field, nil, nil), do: %{}
+  defp query(field, nil, to) do
+    sql = sql(field, "_PARTITIONTIME < @upper")
+    {result, _meta} = BigQuery.Base.Query.query %{upper: to}, sql
+    result_hash(result)
+  end
+  defp query(field, from, to) do
+    sql = sql(field, "_PARTITIONTIME >= @lower AND _PARTITIONTIME < @upper")
+    {result, _meta} = BigQuery.Base.Query.query %{lower: from, upper: to}, sql
+    result_hash(result)
   end
 
-  def combine(parts) do
-    parts
-    |> Enum.sort(&(&1.feeder_episode >= &2.feeder_episode))
-    |> Enum.reduce([], &combine_parts/2)
-  end
-
-  def old_part() do
-    end_dtim = Timex.beginning_of_day(Timex.now())
-    sql = sql("_PARTITIONTIME < @upper")
-    {result, meta} = BigQuery.Base.Query.query %{upper: end_dtim}, sql
-    {end_dtim, result, Map.put(meta, :job, [{nil, end_dtim}])}
-  end
-
-  def mid_part(start_dtim) do
-    end_dtim = Timex.beginning_of_day(Timex.now())
-    if start_dtim == end_dtim do
-      {end_dtim, [], %{cached: true}}
-    else
-      sql = sql("_PARTITIONTIME >= @lower AND _PARTITIONTIME < @upper")
-      {result, meta} = BigQuery.Base.Query.query %{lower: start_dtim, upper: end_dtim}, sql
-      {end_dtim, result, Map.put(meta, :job, [{start_dtim, end_dtim}])}
-    end
-  end
-
-  def new_part(start_dtim) do
-    sql = sql("_PARTITIONTIME >= @lower")
-    {result, meta} = BigQuery.Base.Query.query %{lower: start_dtim}, sql
-    {nil, result, Map.put(meta, :job, [{start_dtim, nil}])}
-  end
-
-  defp sql(where_sql) do
+  defp sql(field, where_sql) do
     """
-      SELECT feeder_podcast, feeder_episode, COUNT(*) AS count
+      SELECT #{field} as key, COUNT(*) AS count
       FROM #{Env.get(:bq_downloads_table)}
-      WHERE is_duplicate = false AND feeder_podcast IS NOT NULL
-      AND feeder_episode IS NOT NULL AND #{where_sql}
-      GROUP BY feeder_podcast, feeder_episode
+      WHERE is_duplicate = false AND #{field} IS NOT NULL AND #{where_sql}
+      GROUP BY #{field}
     """
   end
 
-  defp combine_parts(%{count: n, feeder_episode: guid} = part, [last | rest]) do
-    if last.feeder_episode == guid do
-      [Map.put(last, :count, last.count + n)] ++ rest
-    else
-      [part] ++ [last] ++ rest
-    end
-  end
-  defp combine_parts(part, _), do: [part]
+  defp result_hash(data), do: result_hash(data, %{})
+  defp result_hash([%{count: n, key: key} | rest], acc), do: result_hash(rest, Map.put(acc, key, n))
+  defp result_hash([], acc), do: acc
 end
