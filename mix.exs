f942717eb306f1279f470468369334d9cd77824d
cavis <ryancavis@gmail.com>
2018-04-25 09:56:22 -0600

    Nice to be able to watch tests

--- mix.exs
+++ mix.exs
@@ -59,6 +59,7 @@ defmodule Castle.Mixfile do
      {:prx_auth, "~> 0.0.1"},
      {:memoize, "~> 1.2"},
      {:dotenv, "~> 2.1", only: [:dev, :test]},
+     {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
      {:mock, "~> 0.3.1", only: :test}]
   end
 
