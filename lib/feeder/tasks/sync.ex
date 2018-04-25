e40a4ff6e98b97c637bbb0edc70b3c012349c040
cavis <ryancavis@gmail.com>
2018-04-25 11:38:48 -0600

    Recursive sync

--- lib/feeder/tasks/sync.ex
+++ lib/feeder/tasks/sync.ex
@@ -4,17 +4,36 @@ defmodule Mix.Tasks.Feeder.Sync do
   require Logger
 
   @shortdoc "Manually run the Feeder data refresh"
+  @podlabel "Feeder.SyncPodcasts"
+  @eplabel "Feeder.SyncEpisodes"
 
-  def run(_) do
+  def run(args) do
     {:ok, _started} = Application.ensure_all_started(:castle)
-    Castle.Podcast.max_updated_at()
-      |> log_start("Feeder.SyncPodcasts")
-      |> Feeder.SyncPodcasts.sync()
-      |> log_end("Feeder.SyncPodcasts")
-    Castle.Episode.max_updated_at()
-      |> log_start("Feeder.SyncEpisodes")
-      |> Feeder.SyncEpisodes.sync()
-      |> log_end("Feeder.SyncEpisodes")
+    cond do
+      Enum.member?(args, "--force") || Enum.member?(args, "-f") ->
+        sync_podcasts(nil, true)
+        sync_episodes(nil, true)
+      Enum.member?(args, "--all") || Enum.member?(args, "-a") ->
+        sync_podcasts(Castle.Podcast.max_updated_at(), true)
+        sync_episodes(Castle.Episode.max_updated_at(), true)
+      true ->
+        sync_podcasts(Castle.Podcast.max_updated_at(), false)
+        sync_episodes(Castle.Episode.max_updated_at(), false)
+    end
+  end
+
+  defp sync_podcasts(since, process_all) do
+    status = since |> log_start(@podlabel) |> Feeder.SyncPodcasts.sync() |> log_end(@podlabel)
+    if status == :remaining && process_all do
+      sync_podcasts(Castle.Podcast.max_updated_at(), process_all)
+    end
+  end
+
+  defp sync_episodes(since, process_all) do
+    status = since |> log_start(@eplabel) |> Feeder.SyncEpisodes.sync() |> log_end(@eplabel)
+    if status == :remaining && process_all do
+      sync_episodes(Castle.Episode.max_updated_at(), process_all)
+    end
   end
 
   defp log_start(nil, label) do
@@ -31,5 +50,10 @@ defmodule Mix.Tasks.Feeder.Sync do
   end
   defp log_end({:ok, created, updated, remaining}, label) do
     Logger.info "#{label} #{created} created / #{updated} updated / #{remaining} remaining"
+    if remaining > 0, do: :remaining, else: :done
+  end
+  defp log_end({:error, err}, label) do
+    Logger.error "#{label} #{err}"
+    :error
   end
 end
