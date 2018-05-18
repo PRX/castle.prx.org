1c4367dff8972d5b2d02c36f45532a2355c74c9a
cavis <ryancavis@gmail.com>
2018-05-18 13:51:19 -0600

    Add total directly to trends; support adding cached counts

--- lib/rollup/query/trends.ex
+++ lib/rollup/query/trends.ex
@@ -2,53 +2,72 @@ defmodule Castle.Rollup.Query.Trends do
   import Ecto.Query
 
   def podcast_trends(id, now \\ Timex.now) do
-    today = Timex.beginning_of_day(now)
-    start = Timex.shift(today, days: -13)
+    start = Timex.beginning_of_day(now) |> Timex.shift(days: -13)
 
-    results = Castle.Repo.all from h in Castle.HourlyDownload,
-      where: h.podcast_id == ^id and h.dtim >= ^start and h.dtim <= ^now,
-      select: %{day: fragment("dtim::date"), count: sum(h.count)},
-      group_by: fragment("dtim::date")
+    get_trends = fn ->
+      Castle.Repo.all from h in Castle.HourlyDownload,
+        where: h.podcast_id == ^id and h.dtim >= ^start and h.dtim < ^now,
+        select: %{time: fragment("dtim::date"), count: sum(h.count)},
+        group_by: fragment("dtim::date")
+    end
+    get_total = fn ->
+      Castle.Repo.one from h in Castle.HourlyDownload,
+        where: h.podcast_id == ^id and h.dtim < ^now,
+        select: sum(h.count)
+    end
 
-    format_results(results, today)
+    t1 = Task.async(get_trends)
+    t2 = Task.async(get_total)
+    format_results Task.await(t1), Task.await(t2), now
   end
 
   def episode_trends(id, now \\ Timex.now) do
-    today = Timex.beginning_of_day(now)
-    start = Timex.shift(today, days: -13)
+    start = Timex.beginning_of_day(now) |> Timex.shift(days: -13)
 
-    results = Castle.Repo.all from h in Castle.HourlyDownload,
-      where: h.episode_id == ^id and h.dtim >= ^start and h.dtim <= ^now,
-      select: %{day: fragment("dtim::date"), count: sum(h.count)},
-      group_by: fragment("dtim::date")
+    get_trends = fn ->
+      Castle.Repo.all from h in Castle.HourlyDownload,
+        where: h.episode_id == ^id and h.dtim >= ^start and h.dtim < ^now,
+        select: %{time: fragment("dtim::date"), count: sum(h.count)},
+        group_by: fragment("dtim::date")
+    end
+    get_total = fn ->
+      Castle.Repo.one from h in Castle.HourlyDownload,
+        where: h.episode_id == ^id and h.dtim < ^now,
+        select: sum(h.count)
+    end
 
-    format_results(results, today)
+    t1 = Task.async(get_trends)
+    t2 = Task.async(get_total)
+    format_results Task.await(t1), Task.await(t2), now
   end
 
-  defp format_results(results, today) do
+  def add_cached(trends, cached, now \\ Timex.now) do
+    cached_total = Enum.reduce(cached, 0, fn(%{count: n}, acc) -> acc + n end)
+    cached_trends = format_results(cached, cached_total, now)
     %{
-      today: find_counts(results, today, nil),
-      yesterday: find_counts(results, Timex.shift(today, days: -1), today),
-      this7days: find_counts(results, Timex.shift(today, days: -6), nil),
-      previous7days: find_counts(results, nil, Timex.shift(today, days: -6)),
+      total: trends.total + cached_trends.total,
+      today: trends.today + cached_trends.today,
+      yesterday: trends.yesterday + cached_trends.yesterday,
+      this7days: trends.this7days + cached_trends.this7days,
+      previous7days: trends.previous7days + cached_trends.previous7days,
+    }
+  end
+
+  defp format_results(trends, total, now \\ Timex.now) do
+    today = Timex.beginning_of_day(now)
+    tomorrow = Timex.shift(today, days: 1)
+    %{
+      total: total || 0,
+      today: find_counts(trends, today, tomorrow),
+      yesterday: find_counts(trends, Timex.shift(today, days: -1), today),
+      this7days: find_counts(trends, Timex.shift(today, days: -6), tomorrow),
+      previous7days: find_counts(trends, Timex.shift(today, days: -13), Timex.shift(today, days: -6)),
     }
   end
 
   defp find_counts([], _from, _to), do: 0
-  defp find_counts([%{count: n, day: d} | rest], nil, to) do
-    find_counts(rest, nil, to) + case Timex.compare(d, to) do
-      -1 -> n
-      _ -> 0
-    end
-  end
-  defp find_counts([%{count: n, day: d} | rest], from, nil) do
-    find_counts(rest, from, nil) + case Timex.compare(d, from) do
-      -1 -> 0
-      _ -> n
-    end
-  end
-  defp find_counts([%{count: n, day: d} | rest], from, to) do
-    find_counts(rest, from, to) + case {Timex.compare(d, from), Timex.compare(d, to)} do
+  defp find_counts([%{count: n, time: t} | rest], from, to) do
+    find_counts(rest, from, to) + case {Timex.compare(t, from), Timex.compare(t, to)} do
       {-1, _} -> 0
       {_, -1} -> n
       {_, _} -> 0
--- test/rollup/query/trends_test.exs
+++ test/rollup/query/trends_test.exs
@@ -22,6 +22,7 @@ defmodule Castle.RollupQueryTrendsTest do
 
   test "gets podcast trends", %{now: now} do
     trends = podcast_trends(@id, now)
+    assert trends.total == 35
     assert trends.today == 5
     assert trends.yesterday == 4
     assert trends.this7days == 14
@@ -30,6 +31,7 @@ defmodule Castle.RollupQueryTrendsTest do
 
   test "gets episode trends", %{now: now} do
     trends = episode_trends(@guid, now)
+    assert trends.total == 35
     assert trends.today == 5
     assert trends.yesterday == 4
     assert trends.this7days == 14
@@ -38,10 +40,28 @@ defmodule Castle.RollupQueryTrendsTest do
 
   test "gets lack of trends", %{now: now} do
     trends = podcast_trends(2345, now)
+    assert trends.total == 0
     assert trends.today == 0
     assert trends.yesterday == 0
     assert trends.this7days == 0
     assert trends.previous7days == 0
+  end
+
+  test "adds cached counts to trends data", %{now: now} do
+    trends = podcast_trends(@id, now)
+    cached = [
+      %{count: 100, time: get_dtim("2018-04-25T23:00:00")},
+      %{count: 2, time: get_dtim("2018-04-24T08:00:00")},
+      %{count: 3, time: get_dtim("2018-04-23T23:00:00")},
+      %{count: 4, time: get_dtim("2018-04-11T14:00:00")},
+      %{count: 100, time: get_dtim("2018-04-08T14:00:00")},
+    ]
+    combined_trends = add_cached(trends, cached, now)
+    assert combined_trends.total == trends.total + 209
+    assert combined_trends.today == trends.today + 2
+    assert combined_trends.yesterday == trends.yesterday + 3
+    assert combined_trends.this7days == trends.this7days + 5
+    assert combined_trends.previous7days == trends.previous7days + 4
   end
 
   defp do_insert(dtim_str, count) do
