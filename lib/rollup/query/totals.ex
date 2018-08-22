55ffdcbdb6332c91877201e5f2060fc9a65c17f1
cavis <ryancavis@gmail.com>
2018-08-22 12:23:15 -0600

    Only need totals now

---
+++ lib/rollup/query/totals.ex
@@ -0,0 +1,24 @@
+defmodule Castle.Rollup.Query.Totals do
+  import Ecto.Query
+
+  alias Castle.Rollup.Query.MonthlyDownloads, as: MonthlyDownload
+
+  def podcast_totals(id, now \\ Timex.now) do
+    {count, until_date} = MonthlyDownload.podcast_total_until(id)
+    hour_count = Castle.Repo.one from h in Castle.HourlyDownload, select: sum(h.count),
+      where: h.podcast_id == ^id and h.dtim >= ^Timex.to_datetime(until_date) and h.dtim < ^now
+    count + (hour_count || 0)
+  end
+
+  def episode_totals(id, now \\ Timex.now) do
+    {count, until_date} = MonthlyDownload.episode_total_until(id)
+    hour_count = Castle.Repo.one from h in Castle.HourlyDownload, select: sum(h.count),
+      where: h.episode_id == ^id and h.dtim >= ^Timex.to_datetime(until_date) and h.dtim < ^now
+    count + (hour_count || 0)
+  end
+
+  def add_cached(total, cached_counts) do
+    cached_total = Enum.reduce(cached_counts, 0, fn(%{count: n}, acc) -> acc + n end)
+    total + cached_total
+  end
+end
--- test/rollup/query/totals_test.exs
+++ test/rollup/query/totals_test.exs
@@ -1,8 +1,8 @@
-defmodule Castle.RollupQueryTrendsTest do
+defmodule Castle.RollupQueryTotalsTest do
   use Castle.DataCase, async: false
   use Castle.TimeHelpers
 
-  import Castle.Rollup.Query.Trends
+  import Castle.Rollup.Query.Totals
 
   @id 1234
   @guid "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
@@ -20,35 +20,19 @@ defmodule Castle.RollupQueryTrendsTest do
     [now: now]
   end
 
-  test "gets podcast trends", %{now: now} do
-    trends = podcast_trends(@id, now)
-    assert trends.total == 35
-    assert trends.today == 5
-    assert trends.yesterday == 4
-    assert trends.this7days == 14
-    assert trends.previous7days == 13
+  test "gets podcast totals", %{now: now} do
+    assert podcast_totals(@id, now) == 35
   end
 
   test "gets episode trends", %{now: now} do
-    trends = episode_trends(@guid, now)
-    assert trends.total == 35
-    assert trends.today == 5
-    assert trends.yesterday == 4
-    assert trends.this7days == 14
-    assert trends.previous7days == 13
+    assert episode_totals(@guid, now) == 35
   end
 
   test "gets lack of trends", %{now: now} do
-    trends = podcast_trends(2345, now)
-    assert trends.total == 0
-    assert trends.today == 0
-    assert trends.yesterday == 0
-    assert trends.this7days == 0
-    assert trends.previous7days == 0
+    assert podcast_totals(2345, now) == 0
   end
 
-  test "adds cached counts to trends data", %{now: now} do
-    trends = podcast_trends(@id, now)
+  test "adds cached counts to totals data" do
     cached = [
       %{count: 100, time: get_dtim("2018-04-25T23:00:00")},
       %{count: 2, time: get_dtim("2018-04-24T08:00:00")},
@@ -56,30 +40,22 @@ defmodule Castle.RollupQueryTrendsTest do
       %{count: 4, time: get_dtim("2018-04-11T14:00:00")},
       %{count: 100, time: get_dtim("2018-04-08T14:00:00")},
     ]
-    combined_trends = add_cached(trends, cached, now)
-    assert combined_trends.total == trends.total + 209
-    assert combined_trends.today == trends.today + 2
-    assert combined_trends.yesterday == trends.yesterday + 3
-    assert combined_trends.this7days == trends.this7days + 5
-    assert combined_trends.previous7days == trends.previous7days + 4
+    assert add_cached(35, cached) == 244
   end
 
-  test "adds monthly downloads to trends data", %{now: now} do
+  test "adds monthly downloads to totals data", %{now: now} do
     upsert_monthly(~D[2018-01-01], 100)
 
     upsert_log("monthly_downloads", ~D[2018-01-01], false)
-    trends1 = podcast_trends(@id, now)
+    assert podcast_totals(@id, now) == 35
     upsert_log("monthly_downloads", ~D[2018-01-01], true)
-    trends2 = podcast_trends(@id, now)
-
-    assert trends2.total == trends1.total + 100
+    assert podcast_totals(@id, now) == 135
   end
 
   test "ignores hourly data already covered by monthly", %{now: now} do
     upsert_monthly(~D[2018-04-01], 100)
     upsert_log("monthly_downloads", ~D[2018-04-01], true)
-    trends = podcast_trends(@id, now)
-    assert trends.total == 100
+    assert podcast_totals(@id, now) == 100
   end
 
   defp upsert_hourly(dtim_str, count) do
