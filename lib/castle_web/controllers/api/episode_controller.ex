274085be6183a721ffb9b3f1c0de5097c013b7c7
Sam Vevang <sam.vevang@gmail.com>
2018-11-15 15:55:13 -0600

    mix format

--- lib/castle_web/controllers/api/episode_controller.ex
+++ lib/castle_web/controllers/api/episode_controller.ex
@@ -7,29 +7,38 @@ defmodule CastleWeb.API.EpisodeController do
   def index(%{assigns: %{podcast: podcast}} = conn, params) do
     {page, per} = parse_paging(params)
     {search} = CastleWeb.Search.parse_search(params)
-    queryable = Castle.Episode.recent_query(podcast.id)
-                |> CastleWeb.Search.filter_title_search(search)
+
+    queryable =
+      Castle.Episode.recent_query(podcast.id)
+      |> CastleWeb.Search.filter_title_search(search)
+
     total = Castle.Episode.total(queryable)
     episodes = CastleWeb.Paging.paginated_results(queryable, per, page)
     paging = %{page: page, per: per, total: total, podcast_id: podcast.id, search: search}
-    render conn, "index.json", conn: conn, episodes: episodes, paging: paging
+    render(conn, "index.json", conn: conn, episodes: episodes, paging: paging)
   end
+
   def index(%{prx_user: user} = conn, params) do
     {page, per} = parse_paging(params)
     {search} = CastleWeb.Search.parse_search(params)
     accounts = Map.keys(user.auths)
-    queryable = Castle.Episode.recent_query(accounts)
-                |> CastleWeb.Search.filter_title_search(search)
+
+    queryable =
+      Castle.Episode.recent_query(accounts)
+      |> CastleWeb.Search.filter_title_search(search)
+
     total = Castle.Episode.total(queryable)
     episodes = CastleWeb.Paging.paginated_results(queryable, per, page)
     paging = %{page: page, per: per, total: total, search: search}
-    render conn, "index.json", conn: conn, episodes: episodes, paging: paging
+    render(conn, "index.json", conn: conn, episodes: episodes, paging: paging)
   end
 
   def show(%{assigns: %{episode: episode}} = conn, _params) do
-    total = @redis.episode_totals_cache episode.id, fn(to_dtim) ->
-      Totals.episode_totals(episode.id, to_dtim)
-    end
-    render conn, "show.json", conn: conn, episode: episode, trends: %{total: total}
+    total =
+      @redis.episode_totals_cache(episode.id, fn to_dtim ->
+        Totals.episode_totals(episode.id, to_dtim)
+      end)
+
+    render(conn, "show.json", conn: conn, episode: episode, trends: %{total: total})
   end
 end
--- lib/castle_web/search.ex
+++ lib/castle_web/search.ex
@@ -1,28 +1,28 @@
 defmodule CastleWeb.Search do
-
   import Ecto.Query
 
   def prefix_search(query) do
-
     ends_with_whitespace = Regex.match?(~r/\s$/, query)
     query = String.trim(query)
 
-    query = query
-            |> String.split
-            |> Enum.map(fn term ->
-              # strip all special characters from term
-              term
-              |> String.replace( ~r/[\W]+/u, "")
-            end)
-            |> Enum.join(" ")
-            |> String.trim
+    query =
+      query
+      |> String.split()
+      |> Enum.map(fn term ->
+        # strip all special characters from term
+        term
+        |> String.replace(~r/[\W]+/u, "")
+      end)
+      |> Enum.join(" ")
+      |> String.trim()
 
     # append a suffix wildcard for prefix searching
-    query = if !ends_with_whitespace do
-      query <> ":*"
-    else
-      query
-    end
+    query =
+      if !ends_with_whitespace do
+        query <> ":*"
+      else
+        query
+      end
 
     # Join search terms with an "&" AND
     String.replace(query, ~r/[\s]+/u, "&")
@@ -31,17 +31,20 @@ defmodule CastleWeb.Search do
   def filter_title_search(queryable, query) when is_nil(query) do
     queryable
   end
+
   def filter_title_search(queryable, search_query) do
     queryable
-    |> where(fragment(
-      "to_tsvector('english', coalesce(title, '') || ' ' || coalesce(subtitle, '')) @@ to_tsquery(?)",
-      ^prefix_search(search_query)))
+    |> where(
+      fragment(
+        "to_tsvector('english', coalesce(title, '') || ' ' || coalesce(subtitle, '')) @@ to_tsquery(?)",
+        ^prefix_search(search_query)
+      )
+    )
   end
 
   def parse_search(params) do
     {
-      Map.get(params, "search"),
+      Map.get(params, "search")
     }
   end
-
 end
