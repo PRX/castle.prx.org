bc93c1f07ab5dc288c7b9b0536cb5b9c7636a2c0
Sam Vevang <sam.vevang@gmail.com>
2019-01-15 13:58:52 -0600

    Break out csv dump script into func

--- lib/castle/tasks/dump_hourly.ex
+++ lib/castle/tasks/dump_hourly.ex
@@ -8,31 +8,37 @@ defmodule Mix.Tasks.Castle.DumpHourly do
     {:ok, _started} = Application.ensure_all_started(:castle)
 
     {opts, _, _} = OptionParser.parse args,
-      switches: [path: :string],
-      aliases: [p: :path]
+      switches: [output_dir: :string],
+      aliases: [p: :output_dir]
 
     dump(opts)
   end
 
   def dump([]) do
     IO.puts "\nUsage:"
-    IO.puts "need a `--path` arg\n\n"
+    IO.puts "need a `--output_dir` arg\n\n"
   end
-  def dump([path: path]) when is_binary(path) do
+
+  def dump([path: output_dir]) do
+    dump_hourly_downloads(output_dir)
+  end
+
+  def dump_hourly_downloads(path) do
     Castle.Repo.transaction(fn ->
       query = from h in Castle.HourlyDownload,
+      select: {h.dtim, h.count, h.episode_id, h.podcast_id},
       order_by: [asc: fragment("podcast_id")]
 
       query
       |> Castle.Repo.stream(timeout: :infinity)
       |> Stream.map(fn row ->
-        Map.from_struct(row)
-        |> Map.delete(:__meta__)
-        |> Jason.encode!
+
+        #m = Map.from_struct(row)
+        {dtim, count, episode_id, podcast_id} = row
+        "#{episode_id} #{podcast_id} #{DateTime.to_iso8601(dtim)} #{count}\n"
       end)
       |> Stream.into(File.stream!(path)) |> Stream.run
     end, timeout: :infinity)
-
   end
 
 
