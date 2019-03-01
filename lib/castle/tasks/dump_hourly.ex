bca52a39c42907da8c9f736070224ee41bedb947
Sam Vevang <sam.vevang@gmail.com>
2019-03-01 13:02:18 -0600

    mix format

--- lib/castle/tasks/dump_hourly.ex
+++ lib/castle/tasks/dump_hourly.ex
@@ -7,44 +7,49 @@ defmodule Mix.Tasks.Castle.DumpHourly do
   def run(args) do
     {:ok, _started} = Application.ensure_all_started(:castle)
 
-    {opts, _, _} = OptionParser.parse args,
-      switches: [output_dir: :string],
-      aliases: [p: :output_dir]
+    {opts, _, _} =
+      OptionParser.parse(args,
+        switches: [output_dir: :string],
+        aliases: [p: :output_dir]
+      )
 
     dump(opts)
   end
 
   def dump([]) do
-    IO.puts "\nUsage:"
-    IO.puts "need a `--output_dir` arg\n\n"
+    IO.puts("\nUsage:")
+    IO.puts("need a `--output_dir` arg\n\n")
   end
 
-  def dump([path: output_dir]) do
+  def dump(path: output_dir) do
     dump_hourly_downloads(output_dir)
   end
 
   def hourly_downloads_query do
-    from h in Castle.HourlyDownload,
-    join: e in Castle.Episode,
-    on: e.id == h.episode_id,
-    order_by: [asc: e.podcast_id],
-    select: {h.dtim, h.count, h.episode_id, h.podcast_id, fragment("?::date - ?::date as drop_day_offset", h.dtim, e.published_at)}
+    from(h in Castle.HourlyDownload,
+      join: e in Castle.Episode,
+      on: e.id == h.episode_id,
+      order_by: [asc: e.podcast_id],
+      select:
+        {h.dtim, h.count, h.episode_id, h.podcast_id,
+         fragment("?::date - ?::date as drop_day_offset", h.dtim, e.published_at)}
+    )
   end
 
   def dump_hourly_downloads(path) do
-    Castle.Repo.transaction(fn ->
-
-      hourly_downloads_query()
-      |> Castle.Repo.stream(timeout: :infinity)
-      |> Stream.map(fn row ->
-
-        #m = Map.from_struct(row)
-        {dtim, count, episode_id, podcast_id, drop_day_offset} = row
-        "#{episode_id} #{podcast_id} #{DateTime.to_iso8601(dtim)} #{count} #{drop_day_offset}\n"
-      end)
-      |> Stream.into(File.stream!(path)) |> Stream.run
-    end, timeout: :infinity)
+    Castle.Repo.transaction(
+      fn ->
+        hourly_downloads_query()
+        |> Castle.Repo.stream(timeout: :infinity)
+        |> Stream.map(fn row ->
+          # m = Map.from_struct(row)
+          {dtim, count, episode_id, podcast_id, drop_day_offset} = row
+          "#{episode_id} #{podcast_id} #{DateTime.to_iso8601(dtim)} #{count} #{drop_day_offset}\n"
+        end)
+        |> Stream.into(File.stream!(path))
+        |> Stream.run()
+      end,
+      timeout: :infinity
+    )
   end
-
-
 end
