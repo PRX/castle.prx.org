2f9b8c35bf4a5a72abcfb0f8b802210be18c6ed6
cavis <ryancavis@gmail.com>
2019-06-03 11:01:00 -0600

    Rewrite other rollups to iterate pages

--- test/big_query/rollup/daily_geo_countries_test.exs
+++ test/big_query/rollup/daily_geo_countries_test.exs
@@ -4,15 +4,17 @@ defmodule Castle.BigQueryRollupDailyGeoCountriesTest do
   import BigQuery.Rollup.DailyGeoCountries
 
   test_with_bq "gets empty geo countries in the past", [] do
-    {results, meta} = query(get_dtim("2016-01-01T05:04:00Z"))
-    assert length(results) == 0
+    meta = query get_dtim("2016-01-01T05:04:00Z"), fn(results) ->
+      assert length(results) == 0
+    end
     assert_time meta.day, "2016-01-01T00:00:00Z"
     assert meta.complete == true
   end
 
   test "gets empty geo countries in the future" do
-    {results, meta} = query(get_dtim("2030-01-01"))
-    assert length(results) == 0
+    meta = query get_dtim("2030-01-01"), fn(results) ->
+      assert length(results) == 0
+    end
     assert_time meta.day, "2030-01-01T00:00:00Z"
     assert meta.complete == false
     assert meta.hours_complete == 0
@@ -23,24 +25,26 @@ defmodule Castle.BigQueryRollupDailyGeoCountriesTest do
     %{podcast_id: 2, episode_id: "b", country_iso_code: "US", count: 456},
     %{podcast_id: 1, episode_id: "a", country_iso_code: "US", count: 789},
   ] do
-    {results, meta} = query(get_dtim("2017-05-01"))
-    assert length(results) == 3
+    meta = query get_dtim("2017-05-01"), fn(results) ->
+      assert length(results) == 3
+      assert hd(results).podcast_id == 1
+      assert hd(results).episode_id == "a"
+      assert hd(results).country_iso_code == "US"
+      assert hd(results).count == 123
+      assert hd(results).day == ~D[2017-05-01]
+    end
     assert_time meta.day, "2017-05-01T00:00:00Z"
     assert meta.complete == false
     assert meta.hours_complete == 4
-    assert hd(results).podcast_id == 1
-    assert hd(results).episode_id == "a"
-    assert hd(results).country_iso_code == "US"
-    assert hd(results).count == 123
-    assert hd(results).day == ~D[2017-05-01]
   end
 
   @tag :external
   test "actually gets data" do
-    {results, meta} = query(get_dtim("2017-05-01"))
-    assert length(results) == 20762
+    meta = query get_dtim("2017-05-01"), fn(results) ->
+      assert length(results) == 20762
+      assert hd(results).day == ~D[2017-05-01]
+    end
     assert_time meta.day, "2017-05-01T00:00:00Z"
     assert meta.complete == true
-    assert hd(results).day == ~D[2017-05-01]
   end
 end
--- test/big_query/rollup/daily_geo_metros_test.exs
+++ test/big_query/rollup/daily_geo_metros_test.exs
@@ -4,15 +4,17 @@ defmodule Castle.BigQueryRollupDailyGeoMetrosTest do
   import BigQuery.Rollup.DailyGeoMetros
 
   test_with_bq "gets empty geo metros in the past", [] do
-    {results, meta} = query(get_dtim("2016-01-01T05:04:00Z"))
-    assert length(results) == 0
+    meta = query get_dtim("2016-01-01T05:04:00Z"), fn(results) ->
+      assert length(results) == 0
+    end
     assert_time meta.day, "2016-01-01T00:00:00Z"
     assert meta.complete == true
   end
 
   test "gets empty geo metros in the future" do
-    {results, meta} = query(get_dtim("2030-01-01"))
-    assert length(results) == 0
+    meta = query get_dtim("2030-01-01"), fn(results) ->
+      assert length(results) == 0
+    end
     assert_time meta.day, "2030-01-01T00:00:00Z"
     assert meta.complete == false
     assert meta.hours_complete == 0
@@ -23,25 +25,27 @@ defmodule Castle.BigQueryRollupDailyGeoMetrosTest do
     %{podcast_id: 2, episode_id: "b", metro_code: 8888, count: 456},
     %{podcast_id: 1, episode_id: "a", metro_code: 7777, count: 789},
   ] do
-    {results, meta} = query(get_dtim("2017-05-01"))
-    assert length(results) == 3
+    meta = query get_dtim("2017-05-01"), fn(results) ->
+      assert length(results) == 3
+      assert hd(results).podcast_id == 1
+      assert hd(results).episode_id == "a"
+      assert hd(results).metro_code == 9999
+      assert hd(results).count == 123
+      assert hd(results).day == ~D[2017-05-01]
+    end
     assert_time meta.day, "2017-05-01T00:00:00Z"
     assert meta.complete == false
     assert meta.hours_complete == 4
-    assert hd(results).podcast_id == 1
-    assert hd(results).episode_id == "a"
-    assert hd(results).metro_code == 9999
-    assert hd(results).count == 123
-    assert hd(results).day == ~D[2017-05-01]
   end
 
   @tag :external
   test "actually gets data" do
-    {results, meta} = query(get_dtim("2017-05-01"))
-    assert length(results) == 48440
+    meta = query get_dtim("2017-05-01"), fn(results) ->
+      assert length(results) == 48440
+      assert hd(results).day == ~D[2017-05-01]
+      assert hd(results).metro_code > 0
+    end
     assert_time meta.day, "2017-05-01T00:00:00Z"
     assert meta.complete == true
-    assert hd(results).day == ~D[2017-05-01]
-    assert hd(results).metro_code > 0
   end
 end
