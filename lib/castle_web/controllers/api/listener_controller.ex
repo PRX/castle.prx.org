01b407a3531740ae0feda24676ca1800c74e2baf
Sam Vevang <sam.vevang@gmail.com>
2019-10-31 14:17:07 -0500

    parameterize fetching unique listens from db

--- lib/castle_web/controllers/api/listener_controller.ex
+++ lib/castle_web/controllers/api/listener_controller.ex
@@ -3,10 +3,8 @@ defmodule CastleWeb.API.ListenerController do
 
   alias Castle.Rollup.Query.Uniques, as: Uniques
 
-  @redis Application.get_env(:castle, :redis)
-
-  def index(%{assigns: %{podcast: podcast, interval: intv}} = conn, _params) do
-    data = Uniques.podcast(podcast.id, intv)
-    render conn, "download.json", id: podcast.id, interval: intv, downloads: data
+  def index(%{assigns: %{podcast: podcast, interval: intv}} = conn, params) do
+    data = Uniques.podcast(podcast.id, intv, params)
+    render(conn, "download.json", id: podcast.id, interval: intv, downloads: data)
   end
 end
--- lib/rollup/query/uniques.ex
+++ lib/rollup/query/uniques.ex
@@ -1,27 +1,45 @@
 defmodule Castle.Rollup.Query.Uniques do
   import Ecto.Query
 
-  def podcast(id, %{from: from, to: to, bucket: bucket}) do
-    podcast(id, from, to, bucket.rollup)
-  end
-  def podcast(id, from, to, rollup) do
-    query_podcasts(id, from, to, rollup) |> format_results()
+  def podcast(id, %{from: from, to: to}, %{"interval" => interval_type}) do
+    podcast(id, from, to, interval_type)
   end
 
-  defp query_podcasts(id, from, to, col_name) do
+  def podcast(id, from, to, interval_type) do
     {:ok, from_date} = from |> Timex.format("{YYYY}-{0M}-{0D}")
     {:ok, to_date} = to |> Timex.format("{YYYY}-{0M}-{0D}")
+    query_podcasts(id, from_date, to_date, interval_type) |> format_results()
+  end
 
-    col_name_atom = String.to_atom(col_name)
-    Castle.Repo.all from h in Castle.WeeklyUnique,
-      where: h.podcast_id == ^id and field(h, ^col_name_atom) >= ^from_date and field(h, ^col_name_atom) < ^to_date,
-      select: %{:time => field(h, ^col_name_atom), :count => h.count},
-      order_by: [asc: ^col_name_atom]
+  defp query_podcasts(id, from, to, "LAST_WEEK") do
+    query_listeners_for_interval_type(id, from, to, :week, Castle.LastWeekUnique)
+  end
+
+  defp query_podcasts(id, from, to, "WEEK") do
+    query_listeners_for_interval_type(id, from, to, :week, Castle.WeeklyUnique)
+  end
+
+  defp query_podcasts(id, from, to, "MONTH") do
+    query_listeners_for_interval_type(id, from, to, :month, Castle.MonthlyUnique)
+  end
+
+  defp query_podcasts(id, from, to, "LAST_28") do
+    query_listeners_for_interval_type(id, from, to, :last_28, Castle.Last28Unique)
+  end
+
+  defp query_listeners_for_interval_type(id, from, to, col_name, model) do
+    Castle.Repo.all(
+      from(h in model,
+        where: h.podcast_id == ^id and field(h, ^col_name) >= ^from and field(h, ^col_name) < ^to,
+        select: %{:time => field(h, ^col_name), :count => h.count},
+        order_by: [asc: ^col_name]
+      )
+    )
   end
 
   defp format_results([]), do: []
+
   defp format_results([%{count: n, time: t} | rest]) do
-    IO.puts t
     [%{count: n, time: t}] ++ format_results(rest)
   end
 end
