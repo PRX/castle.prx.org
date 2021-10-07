27e6ae413d32189933641b89eee05ad328ba9b67
cavis <ryancavis@gmail.com>
2021-10-07 13:55:31 -0600

    Just linting

--- test/support/redis_case.ex
+++ test/support/redis_case.ex
@@ -5,7 +5,7 @@ defmodule Castle.RedisCase do
     quote do
       def redis_keys(pattern), do: Castle.Redis.Conn.command(~w(KEYS #{pattern}))
 
-      def redis_count(pattern), do: length redis_keys(pattern)
+      def redis_count(pattern), do: length(redis_keys(pattern))
 
       def redis_clear(pattern), do: redis_keys(pattern) |> Enum.map(&Castle.Redis.Conn.del/1)
     end
