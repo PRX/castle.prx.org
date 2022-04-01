c56e6a0562cb5e2961bd55b95a7e6aa4fe57c583
cavis <ryancavis@gmail.com>
2022-04-01 12:25:34 -0600

    More helpers for running/logging bigquery DML

--- lib/big_query/base/query_params.ex
+++ lib/big_query/base/query_params.ex
@@ -1,20 +1,18 @@
 defmodule BigQuery.Base.QueryParams do
-
-  def parameterize(params \\ %{}) do
-    params
-    |> Map.to_list
-    |> parse_param
-  end
+  def parameterize(), do: parameterize([])
+  def parameterize(params) when is_map(params), do: Map.to_list(params) |> parse_param()
+  def parameterize(params) when is_list(params), do: parse_param(params)
 
   defp parse_param([{key, value} | rest]) do
     [
       %{
         name: key,
         parameterValue: %{value: "#{value}"},
-        parameterType: %{type: guess_type(value)},
+        parameterType: %{type: guess_type(value)}
       }
     ] ++ parse_param(rest)
   end
+
   defp parse_param(_), do: []
 
   defp guess_type(%DateTime{}), do: "TIMESTAMP"
