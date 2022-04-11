fe4f218fd66eb02d378ad538a0d28f50f11e4c8c
cavis <ryancavis@gmail.com>
2022-04-11 08:50:02 -0600

    Stick with castle schema; only do podcast/episode for now

--- priv/big_query/migrations/20220407195034_create_podcast_metadata_tables.exs
+++ priv/big_query/migrations/20220407195034_create_podcast_metadata_tables.exs
@@ -1,34 +1,8 @@
-defmodule BigQuery.Migrations.CreateMetadataTables do
+defmodule BigQuery.Migrations.CreatePodcastMetadataTables do
   alias BigQuery.Base.Query
 
   def up do
     Query.log("""
-      CREATE TABLE agentnames
-      (
-        agentname_id INT64 NOT NULL,
-        tag STRING
-      )
-      OPTIONS(description="PRX Podagent tags");
-
-      CREATE TABLE geonames
-      (
-        geoname_id INT64 NOT NULL,
-        metro_code INT64,
-        metro_name STRING,
-        locale_code STRING,
-        continent_code  STRING,
-        continent_name  STRING,
-        country_iso_code  STRING,
-        country_name  STRING,
-        subdivision_1_iso_code  STRING,
-        subdivision_1_name  STRING,
-        subdivision_2_iso_code  STRING,
-        subdivision_2_name  STRING,
-        city_name STRING,
-        time_zone STRING
-      )
-      OPTIONS(description="Geonames");
-
       CREATE TABLE podcasts
       (
         id INT64 NOT NULL,
@@ -45,7 +19,7 @@ defmodule BigQuery.Migrations.CreateMetadataTables do
 
       CREATE TABLE episodes
       (
-        uuid STRING NOT NULL,
+        id STRING NOT NULL,
         podcast_id INT64,
         title STRING,
         subtitle STRING,
@@ -65,8 +39,6 @@ defmodule BigQuery.Migrations.CreateMetadataTables do
 
   def down do
     Query.log("""
-      DROP TABLE agentnames;
-      DROP TABLE geonames;
       DROP TABLE podcasts;
       DROP TABLE episodes;
     """)
