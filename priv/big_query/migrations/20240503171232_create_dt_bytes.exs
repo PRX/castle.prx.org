bb97f4e102450cbd659bbd6ff52bb36709a0de50
cavis <ryancavis@gmail.com>
2024-05-03 11:18:23 -0600

    Create dt_bytes table

---
+++ priv/big_query/migrations/20240503171232_create_dt_bytes.exs
@@ -0,0 +1,25 @@
+defmodule BigQuery.Migrations.CreateDtDownloads do
+  alias BigQuery.Base.Query
+
+  def up do
+    Query.log("""
+      CREATE TABLE dt_bytes
+      (
+        day DATE,
+        feeder_podcast INT64,
+        feeder_feed STRING,
+        feeder_episode STRING,
+        bytes INT64
+      )
+      PARTITION BY DATE_TRUNC(day, MONTH)
+      OPTIONS(
+        require_partition_filter=true,
+        description="Dovetail CDN bytes usage"
+      )
+    """)
+  end
+
+  def down do
+    raise "WOH- you should never delete dt_bytes!"
+  end
+end
