b643e9b90b8f7a1a7234b60dc42add623e447451
cavis <ryancavis@gmail.com>
2024-07-11 15:20:03 -0600

    Split migrations in half to avoid rate limit

--- priv/big_query/migrations/20240711211814_add_zones_unfilled.exs
+++ priv/big_query/migrations/20240711211814_add_zones_unfilled.exs
@@ -1,15 +1,8 @@
-defmodule BigQuery.Migrations.AddPricing do
+defmodule BigQuery.Migrations.AddZonesUnfilled do
   alias BigQuery.Base.Query
 
   def up do
     Query.log("""
-      ALTER TABLE dt_downloads ADD COLUMN zones_filled_pre INT64;
-      ALTER TABLE dt_downloads ADD COLUMN zones_filled_mid INT64;
-      ALTER TABLE dt_downloads ADD COLUMN zones_filled_post INT64;
-      ALTER TABLE dt_downloads ADD COLUMN zones_filled_house_pre INT64;
-      ALTER TABLE dt_downloads ADD COLUMN zones_filled_house_mid INT64;
-      ALTER TABLE dt_downloads ADD COLUMN zones_filled_house_post INT64;
-
       ALTER TABLE dt_downloads ADD COLUMN zones_unfilled_pre INT64;
       ALTER TABLE dt_downloads ADD COLUMN zones_unfilled_mid INT64;
       ALTER TABLE dt_downloads ADD COLUMN zones_unfilled_post INT64;
@@ -21,20 +14,12 @@ defmodule BigQuery.Migrations.AddPricing do
 
   def down do
     Query.log("""
-      ALTER TABLE dt_downloads DROP COLUMN zones_filled_pre;
-      ALTER TABLE dt_downloads DROP COLUMN zones_filled_mid;
-      ALTER TABLE dt_downloads DROP COLUMN zones_filled_post;
-      ALTER TABLE dt_downloads DROP COLUMN zones_filled_house_pre;
-      ALTER TABLE dt_downloads DROP COLUMN zones_filled_house_mid;
-      ALTER TABLE dt_downloads DROP COLUMN zones_filled_house_post;
-
       ALTER TABLE dt_downloads DROP COLUMN zones_unfilled_pre;
       ALTER TABLE dt_downloads DROP COLUMN zones_unfilled_mid;
       ALTER TABLE dt_downloads DROP COLUMN zones_unfilled_post;
       ALTER TABLE dt_downloads DROP COLUMN zones_unfilled_house_pre;
       ALTER TABLE dt_downloads DROP COLUMN zones_unfilled_house_mid;
       ALTER TABLE dt_downloads DROP COLUMN zones_unfilled_house_post;
-
     """)
   end
 end
