cddd70c5a5b08ea0412c0c4996ea0bd3496bb595
cavis <ryancavis@gmail.com>
2025-08-04 11:03:28 -0600

    Support SSL redis

--- lib/castle/application.ex
+++ lib/castle/application.ex
@@ -5,6 +5,9 @@ defmodule Castle.Application do
 
   use Application
 
+  @default_redis_timeout 500
+  @default_redis_request_timeout 500
+
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
@@ -31,29 +34,51 @@ defmodule Castle.Application do
   end
 
   def redix_clustered_spec do
-    clone_opts =
-      [
-        host: Env.get(:redis_clone_host),
-        port: Env.get(:redis_clone_port),
-        namespace: Env.get(:redis_clone_namespace),
-        pool_size: Env.get(:redis_clone_pool_size)
-      ]
-      |> reject_blank()
-
     opts =
       [
         host: Env.get(:redis_host),
         port: Env.get(:redis_port),
+        ssl: truthy?(Env.get(:redis_ssl)),
         namespace: Env.get(:redis_namespace),
         pool_size: Env.get(:redis_pool_size),
-        clone: clone_opts
+        timeout: Env.get(:redis_timeout) || @default_redis_timeout,
+        request_opts: [timeout: Env.get(:redis_request_timeout) || @default_redis_request_timeout],
+        socket_opts: redix_ssl_socket_opts(truthy?(Env.get(:redis_ssl))),
+        clone: redix_clone_opts()
       ]
       |> reject_blank()
 
     {RedixClustered, opts}
   end
 
+  defp redix_clone_opts do
+    if Env.get(:redis_clone_host) do
+      [
+        host: Env.get(:redis_clone_host),
+        port: Env.get(:redis_clone_port),
+        ssl: truthy?(Env.get(:redis_clone_ssl)),
+        namespace: Env.get(:redis_clone_namespace),
+        pool_size: Env.get(:redis_clone_pool_size),
+        timeout: Env.get(:redis_timeout) || @default_redis_timeout,
+        request_opts: [timeout: Env.get(:redis_request_timeout) || @default_redis_request_timeout],
+        socket_opts: redix_ssl_socket_opts(truthy?(Env.get(:redis_clone_ssl)))
+      ]
+      |> reject_blank()
+    else
+      nil
+    end
+  end
+
   defp reject_blank(opts) do
     Enum.reject(opts, fn {_key, val} -> is_nil(val) || val == "" || val == [] end)
   end
+
+  defp truthy?(val), do: Enum.member?(["TRUE", "true", true, "1", 1], val)
+
+  # required for elasticache: https://hexdocs.pm/redix/Redix.html#module-ssl
+  defp redix_ssl_socket_opts(true) do
+    [customize_hostname_check: [match_fun: :public_key.pkix_verify_hostname_match_fun(:https)]]
+  end
+
+  defp redix_ssl_socket_opts(_), do: []
 end
